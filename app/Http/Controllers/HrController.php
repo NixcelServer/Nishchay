b2a2a4995d565a163f9418f5a59c6563d0b472c3
Harshal Mandale <harshal.mandale@nixcelsoft.com>
2024-03-13 14:45:44 +0530

    Pull main branch

--- app/Http/Controllers/HrController.php
+++ app/Http/Controllers/HrController.php
@@ -153,8 +153,8 @@ class HrController extends Controller
         $kyc_details = KycDetail::where('tbl_user_id',$dec_id)->first();
         $bank_details = BankDetail::where('tbl_user_id',$dec_id)->first();
         $sal_details = SalaryStructureDetail::where('tbl_user_id',$dec_id)->first();
-         
-    
+        
+        
         return view('frontend_hr.editemp',['emp'=>$emp,'user'=>$user,'enc_id'=>$enc_id,'depts'=>$depts,'designations'=>$designations,'roles'=> $roles,'prev_details'=>$prev_details,'managers'=>$managers,'ofc_details'=>$ofc_details,'stat_details'=>$stat_details,'kyc_details'=>$kyc_details,'bank_details'=>$bank_details,'sal_details'=>$sal_details,'mng_name'=>$mng_name]);
     
     
